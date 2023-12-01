f4a0ba4e3bf1eb2fbc05f61683293e2ec84729dc
Sanzhar Suleimenov <s.suleimenov@parqour.com>
2023-12-02 01:11:15 +0600

    initial commit

---
+++ File Server/task/src/server/Main.java
@@ -0,0 +1,30 @@
+package server;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Stage1 stage1 = new Stage1();
+    Scanner scanner = new Scanner(System.in);
+    String command, filename;
+    do {
+      command = scanner.next();
+
+      switch (command) {
+        case "add":
+          filename = scanner.next();
+          stage1.add(filename);
+          break;
+        case "get":
+          filename = scanner.next();
+          stage1.get(filename);
+          break;
+        case "delete":
+          filename = scanner.next();
+          stage1.delete(filename);
+          break;
+      }
+    } while (!"exit".equalsIgnoreCase(command));
+  }
+}
