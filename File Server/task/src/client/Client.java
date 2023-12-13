442ff8b47724e3081d79ea70018fa46218451ca9
Sanzhar Suleimenov <s.suleimenov@parqour.com>
2023-12-13 14:28:25 +0600

    Stage 3 completed.

--- File Server/task/src/client/Client.java
+++ File Server/task/src/client/Client.java
@@ -3,24 +3,82 @@ package client;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
-  private final static String SENT_MESSAGE = "Give me everything you have!";
+  private static final Scanner scanner = new Scanner(System.in);
+
   public static void main(String[] args) {
     try (
-        Socket socket = new Socket("127.0.0.1", 9090);
+        Socket socket = new Socket(InetAddress.getByName("localhost"), 8080);
         DataInputStream inputStream = new DataInputStream(socket.getInputStream());
         DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
-      System.out.println("Client started!");
-      outputStream.writeUTF(SENT_MESSAGE);
-      System.out.println("Sent: " + SENT_MESSAGE);
-      System.out.println("Received: " + inputStream.readUTF());
+
+      System.out.print("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): ");
+      String option = scanner.nextLine().trim().toLowerCase();
+      switch (option) {
+        case "1" -> getFile(inputStream, outputStream);
+        case "2" -> createFile(inputStream, outputStream);
+        case "3" -> deleteFile(inputStream, outputStream);
+        case "exit" -> outputStream.writeUTF("exit");
+        default -> System.err.println("Incorrect option.");
+      }
+
     } catch (IOException e) {
       System.err.println("IOException reason: " + e.getMessage());
     } catch (Exception e) {
       System.err.println("Exception reason: " + e.getMessage());
     }
+  }
+
+  private static void createFile(DataInputStream inputStream, DataOutputStream outputStream)
+      throws IOException {
+    outputStream.writeUTF("PUT");
+    System.out.print("Enter filename: ");
+    String filename = scanner.nextLine();
+    System.out.print("Enter file content: ");
+    String content = scanner.nextLine();
+    outputStream.writeUTF(filename);
+    outputStream.writeUTF(content);
+    int statusCode = inputStream.readInt();
+    if (statusCode == 403) {
+      System.out.println("The response says that creating the file was forbidden!");
+      return;
+    }
+    System.out.println("The response says that file was created!");
+  }
+
+  private static void getFile(DataInputStream inputStream, DataOutputStream outputStream)
+      throws IOException {
+    outputStream.writeUTF("GET");
+    System.out.print("Enter filename: ");
+    String filename = scanner.nextLine();
+    outputStream.writeUTF(filename);
+    System.out.println("The request was sent.");
+    int statusCode = inputStream.readInt();
+    if (statusCode == 404) {
+      System.out.println("The response says that the file was not found!");
+      return;
+    }
+    String content = inputStream.readUTF();
+    System.out.println("The content of the file is: " + content);
+  }
+
+  private static void deleteFile(DataInputStream inputStream, DataOutputStream outputStream)
+      throws IOException {
+    outputStream.writeUTF("DELETE");
+    System.out.print("Enter filename: ");
+    String filename = scanner.nextLine();
+    outputStream.writeUTF(filename);
+    System.out.println("The request was sent.");
+    int statusCode = inputStream.readInt();
+    if (statusCode == 404) {
+      System.out.println("The response says that the file was not found!");
+      return;
+    }
+    System.out.println("The response says that the file was successfully deleted!");
   }
 }
--- File Server/task/src/server/Server.java
+++ File Server/task/src/server/Server.java
@@ -2,31 +2,109 @@ package server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
-  private final static String SENT_MESSAGE = "All files were sent!";
+  private final static String RESOURCE_PATH;
+
+  static {
+    RESOURCE_PATH = System.getProperty("user.dir") +
+        File.separator + "src" +
+        File.separator + "server" +
+        File.separator + "data" +
+        File.separator;
+  }
 
   public static void main(String[] args) {
-    try (ServerSocket serverSocket = new ServerSocket(9090)) {
+    try (ServerSocket serverSocket = new ServerSocket(8080, 50,
+        InetAddress.getByName("localhost"))) {
+      File dir = new File(RESOURCE_PATH);
+      dir.mkdirs();
       System.out.println("Server started!");
-      try (
-          Socket socket = serverSocket.accept();
-          DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-          DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
-        System.out.println("Received: " + inputStream.readUTF());
-        outputStream.writeUTF(SENT_MESSAGE);
-        System.out.println("Sent: " + SENT_MESSAGE);
-      } catch (IOException e) {
-        System.err.println("IOException reason: " + e.getMessage());
+      boolean isActive = true;
+      while (isActive) {
+        try (
+            Socket socket = serverSocket.accept();
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
+
+          String option = inputStream.readUTF().toLowerCase();
+          switch (option) {
+            case "put" -> createFile(inputStream, outputStream);
+            case "get" -> getFile(inputStream, outputStream);
+            case "delete" -> deleteFile(inputStream, outputStream);
+            case "exit" -> isActive = false;
+          }
+        } catch (IOException e) {
+          System.err.println("IOException reason: " + e.getMessage());
+        }
       }
     } catch (IOException e) {
       System.err.println("IOException reason: " + e.getMessage());
     } catch (Exception e) {
       System.err.println("Exception reason: " + e.getMessage());
     }
+  }
+
+  private static void createFile(DataInputStream inputStream, DataOutputStream outputStream)
+      throws IOException {
+    String filename = inputStream.readUTF();
+    String content = inputStream.readUTF();
+    File file = new File(RESOURCE_PATH + filename);
+    boolean createdNew = file.createNewFile();
+    if (createdNew) {
+      try (FileWriter fileWriter = new FileWriter(file)) {
+        fileWriter.write(content);
+        outputStream.writeInt(200);
+      } catch (Exception e) {
+        outputStream.writeInt(403);
+        System.err.println("Exception: " + e.getMessage());
+      }
+    } else {
+      outputStream.writeInt(403);
+    }
+  }
+
+  private static void getFile(DataInputStream inputStream, DataOutputStream outputStream)
+      throws IOException {
+    String filename = inputStream.readUTF();
+    File file = new File(RESOURCE_PATH + filename);
+    if (file.canRead()) {
+      try (Scanner scanner = new Scanner(file)) {
+        String content = scanner.nextLine();
+        outputStream.writeInt(200);
+        outputStream.writeUTF(content);
+      } catch (Exception e) {
+        System.err.println("Exception: " + e.getMessage());
+        outputStream.writeInt(404);
+      }
+    } else {
+      outputStream.writeInt(404);
+    }
+  }
+
+  private static void deleteFile(DataInputStream inputStream, DataOutputStream outputStream)
+      throws IOException {
+    String filename = inputStream.readUTF();
+    File file = new File(RESOURCE_PATH + filename);
+    if (file.isFile()) {
+      try {
+        boolean isDeleted = file.delete();
+        outputStream.writeInt(isDeleted ? 200 : 404);
+      } catch (Exception e) {
+        System.err.println("File delete exception: " + e.getMessage());
+        outputStream.writeInt(404);
+      }
+    } else {
+      outputStream.writeInt(404);
+    }
+
   }
 }
