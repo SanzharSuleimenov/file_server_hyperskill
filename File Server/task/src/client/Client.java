2ed68306f466947bf555c8bea8408cbe4094ff62
Sanzhar Suleimenov <s.suleimenov@parqour.com>
2024-01-07 13:08:39 +0600

    Stage 4. Client side code draft.

--- File Server/task/src/client/Client.java
+++ File Server/task/src/client/Client.java
@@ -2,26 +2,49 @@ package client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Client {
 
   private static final Scanner scanner = new Scanner(System.in);
+  private static final Random random = new Random();
+  private static final String RESOURCE_PATH;
+
+  static {
+    RESOURCE_PATH = System.getProperty("user.dir") +
+        File.separator + "File Server" +
+        File.separator + "task" +
+        File.separator + "src" +
+        File.separator + "client" +
+        File.separator + "data" +
+        File.separator;
+
+    File dir = new File(RESOURCE_PATH);
+    dir.mkdirs();
+  }
 
   public static void main(String[] args) {
+    menu();
+  }
+
+  private static void menu() {
     try (
         Socket socket = new Socket(InetAddress.getByName("localhost"), 8080);
         DataInputStream inputStream = new DataInputStream(socket.getInputStream());
         DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
 
-      System.out.print("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): ");
+      System.out.print("Enter action (1 - get a file, 2 - save a file, 3 - delete a file): ");
       String option = scanner.nextLine().trim().toLowerCase();
       switch (option) {
         case "1" -> getFile(inputStream, outputStream);
-        case "2" -> createFile(inputStream, outputStream);
+        case "2" -> saveFile(inputStream, outputStream);
         case "3" -> deleteFile(inputStream, outputStream);
         case "exit" -> outputStream.writeUTF("exit");
         default -> System.err.println("Incorrect option.");
@@ -34,51 +57,84 @@ public class Client {
     }
   }
 
-  private static void createFile(DataInputStream inputStream, DataOutputStream outputStream)
-      throws IOException {
-    outputStream.writeUTF("PUT");
-    System.out.print("Enter filename: ");
-    String filename = scanner.nextLine();
-    System.out.print("Enter file content: ");
-    String content = scanner.nextLine();
-    outputStream.writeUTF(filename);
-    outputStream.writeUTF(content);
-    int statusCode = inputStream.readInt();
-    if (statusCode == 403) {
-      System.out.println("The response says that creating the file was forbidden!");
-      return;
-    }
-    System.out.println("The response says that file was created!");
-  }
-
   private static void getFile(DataInputStream inputStream, DataOutputStream outputStream)
       throws IOException {
-    outputStream.writeUTF("GET");
-    System.out.print("Enter filename: ");
-    String filename = scanner.nextLine();
-    outputStream.writeUTF(filename);
+    System.out.print("Do you want to get the file by name or by id (1 - name, 2 - id): ");
+    String option = scanner.nextLine();
+    String optionName;
+    switch (option) {
+      case "1" -> {
+        optionName = "name";
+        System.out.print("Enter name: ");
+      }
+      case "2" -> {
+        optionName = "id";
+        System.out.print("Enter id: ");
+      }
+      default -> throw new RuntimeException("Incorrect option value");
+    }
+    String optionValue = scanner.nextLine();
+    outputStream.writeUTF("get");
+    outputStream.writeUTF(optionName);
+    outputStream.writeUTF(optionValue);
     System.out.println("The request was sent.");
     int statusCode = inputStream.readInt();
     if (statusCode == 404) {
-      System.out.println("The response says that the file was not found!");
+      System.out.println("The response says that this file is not found!");
       return;
     }
-    String content = inputStream.readUTF();
-    System.out.println("The content of the file is: " + content);
+    int length = inputStream.readInt();
+    byte[] content = inputStream.readNBytes(length);
+    System.out.print("The file was downloaded! Specify a name for it: ");
+    String filename = scanner.nextLine();
+    Files.write(Path.of(RESOURCE_PATH + filename), content);
+    System.out.println("File save on the hard drive!");
   }
 
   private static void deleteFile(DataInputStream inputStream, DataOutputStream outputStream)
       throws IOException {
-    outputStream.writeUTF("DELETE");
-    System.out.print("Enter filename: ");
-    String filename = scanner.nextLine();
-    outputStream.writeUTF(filename);
+    System.out.print("Do you want to delete the file by name or by id (1 - name, 2 - id): ");
+    String option = scanner.nextLine();
+    String optionName;
+    switch (option) {
+      case "1" -> optionName = "name";
+      case "2" -> optionName = "id";
+      default -> throw new RuntimeException("Incorrect option value");
+    }
+    String optionValue = scanner.nextLine();
+    outputStream.writeUTF("del");
+    outputStream.writeUTF(optionName);
+    outputStream.writeUTF(optionValue);
     System.out.println("The request was sent.");
     int statusCode = inputStream.readInt();
     if (statusCode == 404) {
-      System.out.println("The response says that the file was not found!");
+      System.out.println("The response says that this file is not found!");
       return;
     }
-    System.out.println("The response says that the file was successfully deleted!");
+    System.out.println("The response says that this file was deleted successfully!");
+  }
+
+  private static void saveFile(DataInputStream inputStream, DataOutputStream outputStream)
+      throws IOException {
+    System.out.print("Enter name of the file: ");
+    String filename = scanner.nextLine();
+    File file = new File(RESOURCE_PATH + filename);
+    if (!file.exists()) {
+      System.out.printf("File %s doesn't exist", filename);
+      return;
+    }
+    System.out.print("Enter name of the file to be saved on server: ");
+    String saveFilename = scanner.nextLine();
+    // todo: move name generation to server side
+    if (saveFilename.isBlank()) {
+      saveFilename = "custom_" + random.nextInt(1000) + "_" + System.currentTimeMillis();
+    }
+    outputStream.writeUTF("save"); // represents save action
+    outputStream.writeInt((int) file.length());
+    outputStream.write(Files.readAllBytes(file.toPath()));
+    outputStream.writeUTF(saveFilename);
+    System.out.println("The request was sent.");
+    int fileId = inputStream.readInt();
+    System.out.println("Response says that file is saved! ID = " + fileId);
   }
 }
